d0935176e5955781aa96d060528f4774dd14cee4
GacinhoV33 <fgacek@student.agh.edu.pl>
2023-02-14 01:20:03 +0100

    theme configuration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,21 @@
-function App() {
+import { colorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider, useTheme } from '@mui/material';
+import { tokens } from "./theme";
+
+
+export default function App() {
+  const [theme, colorMode] = useMode();
+  const theme2 = useTheme();
+  const colors = tokens(theme2.palette.mode);
   return (
-    <div className="App">
-      App
-    </div>
+    //@ts-ignore
+    <colorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <div className="app" style={{color: colors.secondary[200]}}>
+            Test
+        </div>
+      </ThemeProvider>
+    </colorModeContext.Provider>
   );
 }
-
-export default App;
--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -1,1 +1,88 @@
-export default 0;
+import { createContext, useState, useMemo } from "react";
+import { createTheme, Theme } from "@mui/material/styles";
+
+export type modeType = 'dark' | 'light';
+
+export const tokens = (mode: modeType) => ({
+    ...(mode === 'dark' ? {
+        primary: {
+            100: "#e0e0e0",
+            200: "#c2c2c2",
+        },
+        secondary: {
+            100: "#d0d1d5",
+            200: "#a1a4ab",
+        }
+    } : {
+        primary: {
+            100: "#e0e0e0",
+            200: "#c2c2c2",
+        },
+        secondary: {
+            100: "#d0d1d5",
+            200: "#a1a4ab",
+        }
+    }
+    )
+})
+
+export const themeSettings = (mode: modeType) => {
+    const colors = tokens(mode);
+    return { 
+        palette: {
+            mode: mode,
+            ...(mode === "dark" ? 
+                {
+                    primary: {
+                        main: colors.primary[100],
+                    },
+                    secondary: {
+                        main: colors.secondary[100],
+                    },
+                    neutral: {
+                        dark: colors.primary[100],
+                        main: colors.primary[100],
+                        light: colors.primary[100],
+                      },
+                      background: {
+                        default: colors.primary[100],
+                      },
+
+                } : { // TODO Colors for dark and light mode 
+                    primary: {
+                        main: colors.primary[100],
+                    },
+                    secondary: {
+                        main: colors.secondary[100],
+                    },
+                    neutral: {
+                        dark: colors.primary[100],
+                        main: colors.primary[100],
+                        light: colors.primary[100],
+                      },
+                      background: {
+                        default: colors.primary[100],
+                      },
+
+                }
+
+            )
+        }
+    }
+}
+
+
+export const colorModeContext = createContext({
+    toggleColorMode: () => {},
+})
+
+export const useMode = (): [Theme, any] => {
+    const [mode, setMode] = useState<modeType>("dark");
+    const colorMode = useMemo(() => (
+        {
+            toggleColorMode: () => setMode((prev) => (prev === 'light' ? "dark" : 'light'))
+        }
+    ), []);
+    const theme: Theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+    return [theme, colorMode];
+}
