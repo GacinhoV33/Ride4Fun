cba825f8dba037f5ff9b0cbfcdbf144ab1da5fc5
GacinhoV33 <fgacek@student.agh.edu.pl>
2023-03-03 20:05:55 +0100

    basic look

--- frontend/src/components/common/Footer.tsx
+++ frontend/src/components/common/Footer.tsx
@@ -1,10 +1,17 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import SocialMedia from './SocialMedia'
-
+import {faHandPointRight} from '@fortawesome/free-regular-svg-icons'
 const Footer: React.FC = () => {
   return (
     <div className='footer-main'>
+      <div style={{display: 'flex', alignItems: 'center'}}>
+        <h2>Sprawdź nasze Social Media </h2>
+        <FontAwesomeIcon icon={faHandPointRight} style={{margin: '0 10px', height: '4vh'}}/>
+      </div>
+      <div style={{position: 'relative', width: '9%', height: '90%', marginLeft: '1.5vw'}}>
         <SocialMedia orientation='row'/>
+      </div>
         <div>
 
         </div>
