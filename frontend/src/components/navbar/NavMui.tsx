b79f5468f4a0ae537209dbd06d375c85c3ae9413
fgacek@student.agh.edu.pl <fgacek@student.agh.edu.pl>
2023-03-22 10:58:04 +0100

    phone styles nav

--- frontend/src/components/navbar/NavMui.tsx
+++ frontend/src/components/navbar/NavMui.tsx
@@ -14,6 +14,7 @@ import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
 import "./NavbarComponent.scss";
 import { Link } from "react-router-dom";
+import './NavbarComponent.scss';
 
 const pages = [
   { menuItem: "Strona Główna", linkPath: "/" },
@@ -32,21 +33,11 @@ function ResponsiveAppBar() {
     null
   );
 
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
+  
   return (
     <AppBar position="fixed" style={{ background: 'transparent' }}>
       <Container maxWidth="xl">
@@ -70,20 +61,20 @@ function ResponsiveAppBar() {
               <img
                 src="./images/Logo_biale.png"
                 alt="logo_navbar"
-                style={{ height: "3.5vw", width: "3.5vw" }}
+                className='nav-img-logo'
               />
             </div>
           </Typography>
 
           <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
           
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+          <Box sx={{ flexGrow: 1, display: {xs: 'flex', md: "flex" } }}>
             {pages.map(({ menuItem, linkPath }, index) => (
               <Link to={linkPath} style={{textDecoration: 'none'}}>
                 <Button
                   key={`${menuItem}-${index}`}
-                  onClick={handleCloseNavMenu}
-                  sx={{ my: 1, color: "white", display: "block", fontSize: '1.25vw', marginRight: '1vw', fontWeight: '500', fontFamily: 'fantasy', letterSpacing: '0.2vw' }}
+                  className='navButton'
+                  sx={{ my: 1, color: "white", display: "block", marginRight: '1vw', fontWeight: '500', fontFamily: 'fantasy', letterSpacing: '0.2vw', zIndex: '20' }}
                 >
                   {menuItem}
                 </Button>
