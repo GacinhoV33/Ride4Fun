2f3367014aaa4a094d9a574886b3cdfdd7d6676c
ist1108957 <filip.gacek@tecnico.ulisboa.pt>
2024-03-05 19:48:20 +0100

    adding motoric trening

--- frontend/src/components/trainings/Trainings.tsx
+++ frontend/src/components/trainings/Trainings.tsx
@@ -4,7 +4,13 @@ import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import PlaceHolder from "../common/PlaceHolder";
 import "./Trainings.scss";
-import { Button, Card, CardActions, CardContent, Typography } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+} from "@mui/material";
 import ContactAcademy, { UserData } from "./ContactAcademy";
 import ImagesPortfolio from "../slides/ImagesPortfolio";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -22,71 +28,126 @@ export interface TrainingProps {
 }
 
 const Trainings: React.FC<TrainingProps> = ({ currentTab, setCurrentTab }) => {
-  const [initialValues, setInitialValues] = useState<UserData>(initialValuesPattern);
+  const [initialValues, setInitialValues] =
+    useState<UserData>(initialValuesPattern);
   const { name } = useParams();
 
   const card1 = (
     <React.Fragment>
       <CardContent>
-        <Typography sx={{ fontSize: 15, fontWeight: 600 }} color="text.secondary" gutterBottom>
+        <Typography
+          sx={{ fontSize: 15, fontWeight: 600 }}
+          color="text.secondary"
+          gutterBottom
+        >
           Grupa początkująca
         </Typography>
         <Typography variant="h5" component="div">
-          Wtorek 16:30
+          Wtorek 17:00
         </Typography>
         <Typography variant="body2">
-        Dzieci i młodzież od 7 roku życia rozpoczynające swoją przygodę <br/> z kolarstwem górskim.
+          Dzieci i młodzież od 7 roku życia rozpoczynające swoją przygodę <br />{" "}
+          z kolarstwem górskim.
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small" onClick={() => handleBasic()}>Zapisz się</Button>
+        <Button size="small" onClick={() => handleBasic()}>
+          Zapisz się
+        </Button>
       </CardActions>
     </React.Fragment>
   );
-  
+
   const card2 = (
     <React.Fragment>
       <CardContent>
-        <Typography sx={{ fontSize: 15, fontWeight: 600 }} color="text.secondary" gutterBottom>
+        <Typography
+          sx={{ fontSize: 15, fontWeight: 600 }}
+          color="text.secondary"
+          gutterBottom
+        >
           Grupa średnio zaawansowana
         </Typography>
         <Typography variant="h5" component="div">
-          Piątek 16:30
+          Poniedziałek 16:30
         </Typography>
         <Typography variant="body2">
-          Dzieci i młodzież chcąca doskonalić umiejętności i wejść na jeszcze wyższy poziom.
+          Dzieci i młodzież chcąca doskonalić umiejętności i wejść na jeszcze
+          wyższy poziom.
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small" onClick={() => handleSemiAdvanced()}>Zapisz się</Button>
+        <Button size="small" onClick={() => handleSemiAdvanced()}>
+          Zapisz się
+        </Button>
       </CardActions>
     </React.Fragment>
   );
 
-  function handleSemiAdvanced(){
+  const card3 = (
+    <React.Fragment>
+      <CardContent>
+        <Typography
+          sx={{ fontSize: 15, fontWeight: 600 }}
+          color="text.secondary"
+          gutterBottom
+        >
+          Trening motoryczny
+        </Typography>
+        <Typography variant="h5" component="div">
+          Czwartek 17:00
+        </Typography>
+        <Typography variant="body2">
+          Zajęcia skupiają się na poprawie koordynacji, wytrzymałości oraz
+          ogólnej sprawności w grawitacyjnych odmianach kolarstwa górskiego.
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" onClick={() => handleMotoric()}>
+          Zapisz się
+        </Button>
+      </CardActions>
+    </React.Fragment>
+  );
+
+  function handleSemiAdvanced() {
     const newInitialValues: UserData = {
-      firstName: '',
-      lastName: '',
-      email: '',
-      contact: '',
-      message: ' ',
-      age: '',
-      level: 'Średnio zaawansowany'
-    }
-    setInitialValues(newInitialValues)
+      firstName: "",
+      lastName: "",
+      email: "",
+      contact: "",
+      message: " ",
+      age: "",
+      level: "Średnio zaawansowany",
+    };
+    setInitialValues(newInitialValues);
     scrollToForm();
   }
 
-  function handleBasic(){
+  function handleBasic() {
     const newInitialValues: UserData = {
-      firstName: '',
-      lastName: '',
-      email: '',
-      contact: '',
-      message: ' ',
-      age: '',
-      level: 'Podstawowy'
-    }
+      firstName: "",
+      lastName: "",
+      email: "",
+      contact: "",
+      message: " ",
+      age: "",
+      level: "Podstawowy",
+    };
+    setInitialValues(newInitialValues);
+    scrollToForm();
+  }
+
+  function handleMotoric() {
+    const newInitialValues: UserData = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      contact: "",
+      message: " ",
+      age: "",
+      level: "Trening motoryczny",
+    };
     setInitialValues(newInitialValues);
     scrollToForm();
   }
@@ -107,18 +168,16 @@ const Trainings: React.FC<TrainingProps> = ({ currentTab, setCurrentTab }) => {
   };
 
   const portfolioRef = useRef<any>(null);
-  const topScrollPortfolio  = portfolioRef.current ? portfolioRef.current.getBoundingClientRect().top : 400;
+  const topScrollPortfolio = portfolioRef.current
+    ? portfolioRef.current.getBoundingClientRect().top
+    : 400;
 
   useEffect(() => {
-   
-
-    if(name === 'r4f'){
+    if (name === "r4f") {
       setCurrentTab(1);
-    }
-    else if(name === 'szkolenia'){
+    } else if (name === "szkolenia") {
       setCurrentTab(0);
-    }
-    else if(name === 'wyjazdy'){
+    } else if (name === "wyjazdy") {
       setCurrentTab(2);
     }
 
@@ -172,6 +231,7 @@ const Trainings: React.FC<TrainingProps> = ({ currentTab, setCurrentTab }) => {
             dzieciaki chcący poznać nową zajawkę, jak i profesjonalni zawodnicy
             przygotowujący się do zawodów.
           </p>
+
           <div className="image-container-trainings">
             <img
               src="./images/szkoleniaInd1.jpg"
@@ -202,12 +262,12 @@ const Trainings: React.FC<TrainingProps> = ({ currentTab, setCurrentTab }) => {
             <div
               style={{ fontFamily: "'Lato', sens-serif", fontSize: "2.75vh" }}
             >
-              1 godzina - <strong>90zł</strong>
+              1 godzina - <strong>100zł</strong>
             </div>
             <div
               style={{ fontFamily: "'Lato', sens-serif", fontSize: "2.75vh" }}
             >
-              2 godziny - <strong>160zł</strong>
+              2 godziny - <strong>180zł</strong>
             </div>
             <div
               style={{
@@ -216,7 +276,7 @@ const Trainings: React.FC<TrainingProps> = ({ currentTab, setCurrentTab }) => {
                 marginBottom: "3vh",
               }}
             >
-              3 godziny - <strong>220zł</strong>
+              3 godziny - <strong>250zł</strong>
             </div>
             <div className="trainings-addInfo">
               Ceny za szkolenie grupowe ustalane są indywidualnie*
@@ -236,7 +296,7 @@ const Trainings: React.FC<TrainingProps> = ({ currentTab, setCurrentTab }) => {
                 itemClass="carousel-item-padding-40-px"
               >
                 {carouselItems.map(({ text, src }, index) => (
-                  <div key={`${src}- ${index}`} className='carousel-cont-img'>
+                  <div key={`${src}- ${index}`} className="carousel-cont-img">
                     <img
                       src={src}
                       alt={`${src}-${index}-alt`}
@@ -260,15 +320,23 @@ const Trainings: React.FC<TrainingProps> = ({ currentTab, setCurrentTab }) => {
               w Całej Polsce!
             </div>
             <div
-              style={{ fontSize: "2.75vh", marginTop: "5vh", fontWeight: "700", fontFamily: "'Lato', sens-serif", textAlign: 'center'
-            }}
+              style={{
+                fontSize: "2.75vh",
+                marginTop: "5vh",
+                fontWeight: "700",
+                fontFamily: "'Lato', sens-serif",
+                textAlign: "center",
+              }}
             >
               Nadal trwa nabór do Szkółki Ride4Fun Academy na cotygodniowe
               treningi dla dzieci i młodzieży!
             </div>
             <h1 style={{ color: "#fafafa" }}>Masz pytania? </h1>
-            <div className='trainings-skontaktuj'>
-              Skontaktuj się z nami w zakładce <Link to="/kontakt" style={{color: '#B70000'}}>KONTAKT</Link>
+            <div className="trainings-skontaktuj">
+              Skontaktuj się z nami w zakładce{" "}
+              <Link to="/kontakt" style={{ color: "#B70000" }}>
+                KONTAKT
+              </Link>
             </div>
           </div>
         </div>
@@ -283,15 +351,38 @@ const Trainings: React.FC<TrainingProps> = ({ currentTab, setCurrentTab }) => {
             tajniki jazdy Enduro i Downhill na rowerze górskim. Zajęcia
             prowadzimy w dwóch grupach zaawansowania, tak by każdy znalazł coś
             dla siebie. Stawiamy nacisk na profesjonalne szkolenie pod czujnym
-            okiem licencjonowanych instruktorów {isMobile ? null : <br/>}z wieloletnim doświadczeniem w
-            pracy z młodzieżą oraz przede wszystkim na uśmiechy i radość z jazdy
-            na rowerze. {isMobile ? null : <br/>}U nas nauczysz się szybko i skutecznie pokonywać leśne
-            szlaki oraz Bike Parkowe przeszkody, a co najważniejsze poznasz
-            nowych rowerowych przyjaciół, co w dalszej rowerowej przygodzie jest
+            okiem licencjonowanych instruktorów {isMobile ? null : <br />}z
+            wieloletnim doświadczeniem w pracy z młodzieżą oraz przede wszystkim
+            na uśmiechy i radość z jazdy na rowerze. {isMobile ? null : <br />}U
+            nas nauczysz się szybko i skutecznie pokonywać leśne szlaki oraz
+            Bike Parkowe przeszkody, a co najważniejsze poznasz nowych
+            rowerowych przyjaciół, co w dalszej rowerowej przygodzie jest
             niezastąpione!
           </p>
+          <div
+            style={{
+              color: "#B70000",
+              fontFamily: "Lato, sans-serif",
+              fontSize: "3vh",
+              fontWeight: "700",
+            }}
+          >
+            NOWOŚĆ!
+          </div>
+          <p
+            className="content-text-trainings"
+            style={{ marginBottom: "50px" }}
+          >
+            Nasze zajęcia treningu motorycznego skupiają się przede wszystkim na
+            poprawie koordynacji, wytrzymałości oraz mają za zadanie stworzyć
+            szeroki i stabilny fundament sprawnościowy . Skupiamy się na
+            wykonywaniu różnorodnych działań ruchowych z akcentem na technikę
+            oraz zadania funkcjonalne, charakterystyczne dla specyfiki jazdy na
+            rowerze w enduro i downhillu. Zajęcia prowadzone są w małych grupach
+            pod certyfikowanych trenerów personalnych oraz fizjoterapeuty.
+          </p>
           <div className="trainings-portfolioImages" ref={portfolioRef}>
-            <ImagesPortfolio topScroll={topScrollPortfolio}/>
+            <ImagesPortfolio topScroll={topScrollPortfolio} />
             <div
               style={{
                 fontSize: "2.5vh",
@@ -317,57 +408,59 @@ const Trainings: React.FC<TrainingProps> = ({ currentTab, setCurrentTab }) => {
                 </Button> */}
               </div>
 
-              <div
-              className='trainings-text-r4f cards-trainings-items'
-              >
-               
-              <Card variant="outlined" className="cards-size-trainings">{card1}</Card>
-              <Card variant="outlined" className="cards-size-trainings">{card2}</Card>
-
+              <div className="trainings-text-r4f cards-trainings-items">
+                <Card variant="outlined" className="cards-size-trainings">
+                  {card2}
+                </Card>
+                <Card variant="outlined" className="cards-size-trainings">
+                  {card1}
+                </Card>
               </div>
-              <div
-              className='trainings-text-r4f'
-              >
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <Card variant="outlined" className="cards-size-trainings">
+                  {card3}
+                </Card>
+              </div>
+              <div className="trainings-text-r4f">
                 <b className="trainings-title-r4f">Lokalizacja: </b> Trenujemy w
                 Bike Park Marcinka zlokalizowanym przy Ul. Harcerskiej w
                 Tarnowie. <br />
+                <div style={isMobile ? {fontSize: '1.45vh'} : {fontSize: '1vh'}}>
+                  *W przypadku deszczu, Trening Motorczyny odbywa się w hali
+                  sportowej
+                </div>
               </div>
-              <div
-              className='trainings-text-r4f'
-              >
+              <div className="trainings-text-r4f">
                 <b className="trainings-title-r4f">Grupy: </b>
-                Początkująca i Średniozaawansowana {isMobile ? null : <br/>}dla dzieci od 7 roku życia.
-                Młodszych adeptów zapraszamy na zajęcia indywidualne! <br />
+                Początkująca i Średniozaawansowana {isMobile ? null : <br />}dla
+                dzieci od 7 roku życia. Młodszych adeptów zapraszamy na zajęcia
+                indywidualne! <br />
               </div>
-              <div
-              className='trainings-text-r4f'
-              >
+              <div className="trainings-text-r4f">
                 {" "}
                 <b className="trainings-title-r4f">Czas trwania: </b> Trening
                 trwa dwie godziny. <br />{" "}
+                <div style={isMobile ? {fontSize: '1.45vh'} : {fontSize: '1vh'}}>
+                  *Trening motoryczny trwa jedną godzinę i odbywa się co dwa tygodnie
+                </div>
               </div>
 
-              <div
-              className='trainings-text-r4f'
-              >
-                <b
-                                className="trainings-title-r4f"
-
-                >
-                  Cena:{" "}
-                </b>{" "}
-                200pln/miesiąc <br />
+              <div className="trainings-text-r4f">
+                <b className="trainings-title-r4f">Cena: </b> 250pln/miesiąc{" "}
+                <br />
+                <div style={isMobile ? {fontSize: '1.45vh'} : {fontSize: '1vh'}}>
+                  *Cena zajęć rozszerzona o trening motoryczny realizowany raz na dwa tygodnie <br></br>wynosi 350pln
+                </div>
               </div>
+              
             </div>
           </div>
-      
+
           <ContactAcademy initialValues={initialValues} />
           <div ref={contactRef}></div>
         </div>
       )}
-      {currentTab === 2 && (
-        <Trips/>
-      )}
+      {currentTab === 2 && <Trips />}
     </Box>
   );
 };
@@ -413,14 +506,12 @@ export const responsive = {
   },
 };
 
-
-
 const initialValuesPattern: UserData = {
-  firstName: '',
-  lastName: '',
-  email: '',
-  contact: '',
-  message: ' ',
-  age: '',
-  level: ''
-}
+  firstName: "",
+  lastName: "",
+  email: "",
+  contact: "",
+  message: " ",
+  age: "",
+  level: "",
+};
